7284c13e7342988c6d7df7b9ca880c421be55ce6
alepeculio <alejandropeculio@gmail.com>
2018-05-04 22:29:24 -0300

    Merge branch 'master' of https://github.com/Jorge1701/BicicletasAdmin
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/jorge/testgithub/MenuAdmin.java
+++ app/src/main/java/com/example/jorge/testgithub/MenuAdmin.java
@@ -2,8 +2,10 @@ package com.example.jorge.testgithub;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,7 +19,7 @@ import android.widget.Toast;
 import butterknife.ButterKnife;
 
 public class MenuAdmin extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, UsuariosListado.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,12 +135,16 @@ public class MenuAdmin extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        boolean fragmentTransaction = false;
+        Fragment fragment = null;
+
         switch (id) {
             case R.id.nav_ver_mapa:
 
                 break;
             case R.id.nav_usuarios:
-
+                fragment = new UsuariosListado();
+                fragmentTransaction = true;
                 break;
             case R.id.nav_paradas:
 
@@ -154,8 +160,21 @@ public class MenuAdmin extends AppCompatActivity
                 break;
         }
 
+        if(fragmentTransaction){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contenido_seleccionado,fragment)
+                    .commit();
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
