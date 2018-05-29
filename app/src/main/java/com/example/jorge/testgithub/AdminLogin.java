394bad39ea0fd8918da37a41b5e37947014bc276
Jorge <rosasjorge1701@gmail.com>
2018-05-28 22:44:11 -0300

    Login conectao con BD

--- app/src/main/java/com/example/jorge/testgithub/AdminLogin.java
+++ app/src/main/java/com/example/jorge/testgithub/AdminLogin.java
@@ -1,7 +1,9 @@
 package com.example.jorge.testgithub;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
