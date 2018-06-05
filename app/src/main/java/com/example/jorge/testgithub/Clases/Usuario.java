dfec5ade6d8fda42aa963774eb22c308932997f6
alepeculio <alejandropeculio@gmail.com>
2018-06-05 13:55:10 -0300

    la puta madre

--- app/src/main/java/com/example/jorge/testgithub/Clases/Usuario.java
+++ app/src/main/java/com/example/jorge/testgithub/Clases/Usuario.java
@@ -2,6 +2,7 @@ package com.example.jorge.testgithub.Clases;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.jorge.testgithub.BD.BDCliente;
 import com.example.jorge.testgithub.BD.BDInterface;
@@ -19,181 +20,181 @@ import com.example.jorge.testgithub.AdaptadorListaUsuarios.UsuarioViewHolder;
 
 public class Usuario {
 
-	private String Email;
-	private String Cedula;
-	private String Pasaporte;
-	private String Pass;
-	private String Nombre;
-	private String Telefono;
-	private String Direccion;
-	private int Activado;
-	private int Saldo;
-	private int IdCelular;
-	private int cantidadAlquileres;
+    private String Email;
+    private String Cedula;
+    private String Pasaporte;
+    private String Pass;
+    private String Nombre;
+    private String Telefono;
+    private String Direccion;
+    private int Activado;
+    private int Saldo;
+    private int IdCelular;
+    private int cantidadAlquileres;
 
-	public Usuario(String email, String cedula, String pasaporte, String pass, String nombre, String telefono, String direccion, int activado, int saldo, int idCelular, int cantidadAlquileres) {
-		Email = email;
-		Cedula = cedula;
-		Pasaporte = pasaporte;
-		Pass = pass;
-		Nombre = nombre;
-		Telefono = telefono;
-		Direccion = direccion;
-		Activado = activado;
-		Saldo = saldo;
-		IdCelular = idCelular;
-		this.cantidadAlquileres = cantidadAlquileres;
-	}
+    public Usuario(String email, String cedula, String pasaporte, String pass, String nombre, String telefono, String direccion, int activado, int saldo, int idCelular, int cantidadAlquileres) {
+        Email = email;
+        Cedula = cedula;
+        Pasaporte = pasaporte;
+        Pass = pass;
+        Nombre = nombre;
+        Telefono = telefono;
+        Direccion = direccion;
+        Activado = activado;
+        Saldo = saldo;
+        IdCelular = idCelular;
+        this.cantidadAlquileres = cantidadAlquileres;
+    }
 
-	public String getEmail() {
-		return Email;
-	}
+    public String getEmail() {
+        return Email;
+    }
 
-	public void setEmail(String email) {
-		Email = email;
-	}
+    public void setEmail(String email) {
+        Email = email;
+    }
 
-	public String getCedula() {
-		return Cedula;
-	}
+    public String getCedula() {
+        return Cedula;
+    }
 
-	public void setCedula(String cedula) {
-		Cedula = cedula;
-	}
+    public void setCedula(String cedula) {
+        Cedula = cedula;
+    }
 
-	public String getPasaporte() {
-		return Pasaporte;
-	}
+    public String getPasaporte() {
+        return Pasaporte;
+    }
 
-	public void setPasaporte(String pasaporte) {
-		Pasaporte = pasaporte;
-	}
+    public void setPasaporte(String pasaporte) {
+        Pasaporte = pasaporte;
+    }
 
-	public String getPass() {
-		return Pass;
-	}
+    public String getPass() {
+        return Pass;
+    }
 
-	public void setPass(String pass) {
-		Pass = pass;
-	}
+    public void setPass(String pass) {
+        Pass = pass;
+    }
 
-	public String getNombre() {
-		return Nombre;
-	}
+    public String getNombre() {
+        return Nombre;
+    }
 
-	public void setNombre(String nombre) {
-		Nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
 
-	public String getTelefono() {
-		return Telefono;
-	}
+    public String getTelefono() {
+        return Telefono;
+    }
 
-	public void setTelefono(String telefono) {
-		Telefono = telefono;
-	}
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
 
-	public String getDireccion() {
-		return Direccion;
-	}
+    public String getDireccion() {
+        return Direccion;
+    }
 
-	public void setDireccion(String direccion) {
-		Direccion = direccion;
-	}
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
 
-	public int isActivado() {
-		return Activado;
-	}
+    public int isActivado() {
+        return Activado;
+    }
 
-	public void setActivado(int activado) {
-		Activado = activado;
-	}
+    public void setActivado(int activado) {
+        Activado = activado;
+    }
 
-	public int getSaldo() {
-		return Saldo;
-	}
+    public int getSaldo() {
+        return Saldo;
+    }
 
-	public void setSaldo(int saldo) {
-		Saldo = saldo;
-	}
+    public void setSaldo(int saldo) {
+        Saldo = saldo;
+    }
 
-	public int getIdCelular() {
-		return IdCelular;
-	}
+    public int getIdCelular() {
+        return IdCelular;
+    }
 
-	public void setIdCelular(int idCelular) {
-		IdCelular = idCelular;
-	}
+    public void setIdCelular(int idCelular) {
+        IdCelular = idCelular;
+    }
 
-	public int getCantidadAlquileres() {
-		return cantidadAlquileres;
-	}
+    public int getCantidadAlquileres() {
+        return cantidadAlquileres;
+    }
 
-	public void setCantidadAlquileres(int cantidadAlquileres) {
-		this.cantidadAlquileres = cantidadAlquileres;
-	}
+    public void setCantidadAlquileres(int cantidadAlquileres) {
+        this.cantidadAlquileres = cantidadAlquileres;
+    }
 
-	public void habilitar (final UsuarioViewHolder v) {
-		BDInterface bd = BDCliente.getClient().create(BDInterface.class);
-		Call<Respuesta> call = bd.habilitar(Email);
-		call.enqueue(new Callback<Respuesta>() {
-			@Override
-			public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
-				if (response.body().getCodigo().equals("1")) {
-					v.barraProgreso.setVisibility (View.GONE);
-					v.btnHabilitar.setVisibility(View.GONE);
-					v.btnInhabilitar.setVisibility(View.VISIBLE);
-					Activado = 1;
-				}
-			}
+    public void habilitar(final UsuarioViewHolder v) {
+        BDInterface bd = BDCliente.getClient().create(BDInterface.class);
+        Call<Respuesta> call = bd.habilitar(Email);
+        call.enqueue(new Callback<Respuesta>() {
+            @Override
+            public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
+                if (response.body().getCodigo().equals("1")) {
+                    v.barraProgreso.setVisibility(View.GONE);
+                    v.btnHabilitar.setVisibility(View.GONE);
+                    v.btnInhabilitar.setVisibility(View.VISIBLE);
+                    Activado = 1;
+                }
+            }
 
-			@Override
-			public void onFailure(Call<Respuesta> call, Throwable t) {
-				v.barraProgreso.setVisibility (View.GONE);
-				v.btnHabilitar.setVisibility(View.VISIBLE);
-				v.btnInhabilitar.setVisibility(View.GONE);
-				Log.d ("FALLO", t.getMessage ());
-			}
-		});
-	}
+            @Override
+            public void onFailure(Call<Respuesta> call, Throwable t) {
+                v.barraProgreso.setVisibility(View.GONE);
+                v.btnHabilitar.setVisibility(View.VISIBLE);
+                v.btnInhabilitar.setVisibility(View.GONE);
+                Log.d("FALLO", t.getMessage());
+            }
+        });
+    }
 
-	public void inhabilitar (final UsuarioViewHolder v) {
-		BDInterface bd = BDCliente.getClient().create(BDInterface.class);
-		Call<Respuesta> call = bd.inhabilitar(Email);
-		call.enqueue(new Callback<Respuesta>() {
-			@Override
-			public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
-				if (response.body().getCodigo().equals("1")) {
-					v.barraProgreso.setVisibility (View.GONE);
-					v.btnHabilitar.setVisibility (View.VISIBLE);
-					v.btnInhabilitar.setVisibility (View.GONE);
-					Activado = 0;
-				}
-			}
+    public void inhabilitar(final UsuarioViewHolder v) {
+        BDInterface bd = BDCliente.getClient().create(BDInterface.class);
+        Call<Respuesta> call = bd.inhabilitar(Email);
+        call.enqueue(new Callback<Respuesta>() {
+            @Override
+            public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
+                if (response.body().getCodigo().equals("1")) {
+                    v.barraProgreso.setVisibility(View.GONE);
+                    v.btnHabilitar.setVisibility(View.VISIBLE);
+                    v.btnInhabilitar.setVisibility(View.GONE);
+                    Activado = 0;
+                }
+            }
 
-			@Override
-			public void onFailure(Call<Respuesta> call, Throwable t) {
-				v.barraProgreso.setVisibility (View.GONE);
-				v.btnHabilitar.setVisibility (View.GONE);
-				v.btnInhabilitar.setVisibility (View.VISIBLE);
-				Log.d ("FALLO", t.getMessage ());
-			}
-		});
-	}
+            @Override
+            public void onFailure(Call<Respuesta> call, Throwable t) {
+                v.barraProgreso.setVisibility(View.GONE);
+                v.btnHabilitar.setVisibility(View.GONE);
+                v.btnInhabilitar.setVisibility(View.VISIBLE);
+                Log.d("FALLO", t.getMessage());
+            }
+        });
+    }
 
-	public static void verificarUsuario (final Login l, final String usuario, final String password) {
-		BDInterface bd = BDCliente.getClient().create(BDInterface.class);
-		Call<Respuesta> call = bd.login (usuario, password, null);
-		call.enqueue(new Callback<Respuesta>() {
-			@Override
-			public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
-				l.verificarLogin(response.body().getCodigo().equals("1"), usuario, password);
-			}
+    public static void verificarUsuario(final Login l, final String usuario, final String password) {
+        BDInterface bd = BDCliente.getClient().create(BDInterface.class);
+        Call<Respuesta> call = bd.login(usuario, password,"administrador");
+        call.enqueue(new Callback<Respuesta>() {
+            @Override
+            public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
+                l.verificarLogin(response.body().getCodigo().equals("1"), usuario, password);
+            }
 
-			@Override
-			public void onFailure(Call<Respuesta> call, Throwable t) {
-				Log.d ("FALLO", t.getMessage ());
-			}
-		});
-	}
+            @Override
+            public void onFailure(Call<Respuesta> call, Throwable t) {
+                Log.d("Error","No se pudo conectar al servidor");
+            }
+        });
+    }
 }
